82b1204f4ee1a4a0fbd24905c0be733e9e855541
mazh91 <milad.azh@ryerson.ca>
2019-08-01 13:29:17 -0400

    add /health endpoint

---
+++ src/main/java/ca/jrvs/apps/trading/controller/AppController.java
@@ -0,0 +1,19 @@
+package ca.jrvs.apps.trading.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/health")
+public class AppController {
+
+    @ApiOperation("Check application health")
+    @GetMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String checkHealth() {
+        return "App is up and running.";
+    }
+}
