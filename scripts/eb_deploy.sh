8763abd9c1e5e68bb8a08b9f75ec67bcdaf06712
mazh91 <milad.azh@ryerson.ca>
2019-08-08 19:34:41 -0400

    correct artifact path

--- scripts/eb_deploy.sh
+++ scripts/eb_deploy.sh
@@ -20,7 +20,7 @@ eb use ${eb_env}
 #Edit EB config file which tells EB which artifact to deploy
 cat >> .elasticbeanstalk/config.yml <<_EOF
 deploy:
-  artifact: target/trading-1.0-SNAPSHOT-elastic-beanstalk.zip
+  artifact: target/trading-1.0-SNAPSHOT.zip
 _EOF
 
 #deploy
